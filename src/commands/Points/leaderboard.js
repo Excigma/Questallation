1ceaacb8350e2177446b98d32928f759a77475c8
ExcelledBot <30280397+Excigma@users.noreply.github.com>
2019-01-24 20:10:14 +1300

    Add placing

--- src/commands/Points/leaderboard.js
+++ src/commands/Points/leaderboard.js
@@ -12,24 +12,23 @@ module.exports = class extends Command {
 
 	async run(message) {
 		const display = new RichDisplay();
-		let place = 1;
 		const data = this.client.users
 			.filter(user => user.settings.experience)
-			.sort((a, b) => a.settings.experience < b.settings.experience ? 1 : -1)
-			.map(user => `(${place++}) ${user.tag}: ${user.settings.experience}`).join("\n");
-
-		const chunks = Util.splitMessage(data, { char: "\n", maxLength: 500 });
+			.sort((a, b) => b.settings.experience - a.settings.experience).array();
+		const sorted = data.map((user, place) => `(${place + 1}) ${user.tag}: ${user.settings.experience}`).join("\n");
+		console.log(data);
+		const chunks = Util.splitMessage(sorted, { char: "\n", maxLength: 100 });
 
 
 		if (typeof chunks === "object") {
 			for (const chunk of chunks) {
 				display.addPage(new MessageEmbed(message.excigmaEmbed)
-					.setDescription(chunk));
+					.setDescription(`You are placing ${data.indexOf(message.author) + 1}\n${chunk}`));
 			}
 			return display.run(await message.send("Loading data..."));
 		} else {
 			return message.sendEmbed(new MessageEmbed(message.excigmaEmbed)
-				.setDescription(chunks));
+				.setDescription(`You are placing ${data.indexOf(message.author) + 1}\n${chunks}`));
 		}
 	}
 };
