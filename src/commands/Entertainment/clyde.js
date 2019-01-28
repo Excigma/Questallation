db7f71a82f40b6ca07295a326d665bdbdfae41ee
ExcelledBot <30280397+Excigma@users.noreply.github.com>
2019-01-28 16:21:38 +1300

    Show who levels up

--- src/commands/Entertainment/clyde.js
+++ src/commands/Entertainment/clyde.js
@@ -25,7 +25,7 @@ module.exports = class extends Command {
         });
 
         await webhook.delete();
-        return msg.delete().catch(e => {});
+        return msg.delete().catch();
     }
 
 };
--- src/website.js
+++ src/website.js
@@ -1,12 +1,35 @@
 /* eslint-disable callback-return*/
 const { resolve } = require("path"),
-    express = require("express"), { renderFile } = require("ejs"),
-    compression = require("compression"),
-    mcache = require("memory-cache"), { version: klasaVersion, Duration } = require(`${process.cwd()}/src/index`);
+    express = require("express"),
+    compression = require("compression");
 
-module.exports = async(client) => {
+module.exports = async (client) => {
     client.console.debug("Website: Initiating");
-    var commands = {};
+    /*
+    const mcache = require("memory-cache");
+        var cache = () => {
+            return (req, res, next) => {
+                if (process.env.NODE_ENV === "production") {
+                    const key = `__express__${req.originalUrl}` || req.url;
+                    const cachedBody = mcache.get(key);
+                    if (cachedBody) {
+                        res.send(cachedBody);
+                        return;
+                    } else {
+                        res.sendResponse = res.send;
+                        res.send = (body) => {
+                            mcache.put(key, body);
+                            res.sendResponse(body);
+                        };
+                        next();
+                    }
+                } else {
+                    next();
+                }
+            };
+        };
+
+        var commands = {};
 
     for (const cmd of client.commands.values()) {
         if (!commands.hasOwnProperty(cmd.category)) commands[cmd.category] = {};
@@ -16,37 +39,11 @@ module.exports = async(client) => {
             usage: cmd.usageString
         };
     }
-
-    var cache = () => {
-        return (req, res, next) => {
-            if (process.env.NODE_ENV === "production") {
-                const key = `__express__${req.originalUrl}` || req.url;
-                const cachedBody = mcache.get(key);
-                if (cachedBody) {
-                    res.send(cachedBody);
-                    return;
-                } else {
-                    res.sendResponse = res.send;
-                    res.send = (body) => {
-                        mcache.put(key, body);
-                        res.sendResponse(body);
-                    };
-                    next();
-                }
-            } else {
-                next();
-            }
-        };
-    };
-
+    */
 
     await express()
         .use(compression({ threshold: 1 }))
         .enable("etag")
-        .use((req, res, next) => {
-            res.set();
-            next();
-        })
         .use("/", express.static(resolve("./src/public")));
     /*
     .engine("html", renderFile)
@@ -60,7 +57,7 @@ module.exports = async(client) => {
     /* eslint-disable-next-line no-unused-vars * /
     .use((err, req, res, next) => { res.status(500); res.render(resolve("./src/pages/500.ejs"), { path: "/500 - Internal server error" }); console.error(err); })
     .use((req, res) => { res.status(404); res.render(resolve("./src/pages/404.ejs"), { path: "/404 - Page not found" }); })
-    	.listen(process.env.PORT || "8080", () => client.console.debug(`Website: Ready on port ${process.env.PORT || "8080"}`));*/
+        .listen(process.env.PORT || "8080", () => client.console.debug(`Website: Ready on port ${process.env.PORT || "8080"}`));*/
 
 
 };
