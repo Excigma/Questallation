4ad9816d2b3919fdacdcd03ad4b5083b89806638
Excigma <30280397+Excigma@users.noreply.github.com>
2019-02-09 23:08:44 +1300

    Bye opusscript, I can have node opus on my latop now

--- src/commands/Text/binary.js
+++ src/commands/Text/binary.js
@@ -1,30 +1,17 @@
 const { TextCommand } = require(`${process.cwd()}/src/index`);
-const binary = (string) => {
-    return string.split("").map((char) => {
-        return char.charCodeAt(0).toString(2);
-    }).join(" ");
-};
+const binary = (string) => string.split("").map((char) => { return char.charCodeAt(0).toString(2); }).join(" ");
+
 
 module.exports = class extends TextCommand {
     constructor(...args) {
         super(...args, {
             description: "Makes binary from words.",
             usage: "<Text:str>",
-            extendedHelp: "This command changes a tring to binary."
+            extendedHelp: "This command changes a string to binary."
         });
     }
 
-    async run(message, [str]) {
-        message.sendEmbed({
-            title: "Binary",
-            color: 0x7289DA,
-            author: {
-                name: message.author.tag,
-                icon_url: message.author.avatarURL()
-            },
-            description: binary(str)
-        });
-
+    async run(msg, [str]) {
+        this.sendTest(binary(str));
     }
-
 };
